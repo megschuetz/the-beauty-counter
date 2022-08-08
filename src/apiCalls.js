a76913d6306b6665c73d46fd88a24a5fc5352d9e
Megan Schuetz <mschuetz411@gmail.com>
2022-08-07 18:57:52 -0600

    add favicon

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,10 +10,10 @@ function fetchResponse(url) {
   );
 };
 
-const NYX = fetchResponse('http://makeup-api.herokuapp.com/api/v1/products.json?brand=nyx');
-const maybelline = fetchResponse('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
-const milani = fetchResponse('http://makeup-api.herokuapp.com/api/v1/products.json?brand=milani');
-const clinique = fetchResponse('http://makeup-api.herokuapp.com/api/v1/products.json?brand=clinique');
-const covergirl = fetchResponse('http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl');
+const NYX = fetchResponse('https://makeup-api.herokuapp.com/api/v1/products.json?brand=nyx');
+const maybelline = fetchResponse('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
+const milani = fetchResponse('https://makeup-api.herokuapp.com/api/v1/products.json?brand=milani');
+const clinique = fetchResponse('https://makeup-api.herokuapp.com/api/v1/products.json?brand=clinique');
+const covergirl = fetchResponse('https://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl');
 
 export { NYX, maybelline, milani, clinique, covergirl }
